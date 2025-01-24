629869f475aa1968092d96a16ca3fcdd78d0585c
Gfecito <71032552+Gfecito@users.noreply.github.com>
2025-01-24 12:53:32 -0500

    webserver

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM debian:stable-slim
 # COPY source destination
 COPY go_web_server /bin/go_web_server
 
+ENV PORT=8080
 CMD [ "/bin/go_web_server" ]
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -12,7 +13,10 @@ func main() {
 
 	m.HandleFunc("/", handlePage)
 
-	const addr = ":8080"
+	port := os.Getenv("PORT")
+
+	// we type this var as a string
+	var addr string = ":" + port
 	srv := http.Server{
 		Handler:      m,
 		Addr:         addr,
